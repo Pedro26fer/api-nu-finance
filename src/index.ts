508d8f531bb8d47b2a74ddfe8fa48598ade8c9e1
Pedro26fer <p2698fernandes@gmail.com>
2023-05-09 22:58:56 -0300

    fix: Error

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,35 @@
 import express from 'express'
-import cors from 'cors'
 import { userRoutes } from './routes/userRoutes.routes'
 import { globalErrorMiddleware } from './middlewares/globalError.middleware'
 import { accountsRoutes } from './routes/accountsRoutes.routes'
 import { transactionsRoute } from './routes/transactionsRoute.routes'
 import { sessionRoutes } from './routes/sessionRoute.routes'
+import AppDataSource from './data-source'
+
+
+AppDataSource.initialize()
+.then(() => {
+
+    const app = express()
+    
+    app.use(express.json())
+    
+    app.use(globalErrorMiddleware)
+
+    userRoutes(app)
+    sessionRoutes(app)
+    accountsRoutes(app)
+    transactionsRoute(app)
+
+    return app.listen(process.env.PORT || 3000)
+  
+
+})
+.catch((err) => console.log("Erro na execução do bnco", err))
 
 
 
 
 
-const app = express()
 
-app.set("port", process.env.PORT || 3000)
-
-app.use(express.json())
-
-app.use(globalErrorMiddleware)
-
-app.use(cors())
-
-
-userRoutes(app)
-sessionRoutes(app)
-accountsRoutes(app)
-transactionsRoute(app)
-
-
-export default app
 
