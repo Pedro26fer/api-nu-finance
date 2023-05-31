dc991f33fc20d3bec8dec412ada355bdeac0d5e5
Pedro26fer <p2698fernandes@gmail.com>
2023-05-31 19:13:03 -0300

    feat: identation code

--- src/controllers/user/updateUser.controller.ts
+++ src/controllers/user/updateUser.controller.ts
@@ -3,16 +3,16 @@ import updateUserService from "../../services/user/updateUser.service";
 import { AppError, handleError } from "../../error/appError";
 
 const updateUserController = async (req: Request, res: Response) => {
-    try {
-        const {id} = req.params
-        const data = req.body 
-        await updateUserService(id, data)
-        return res.status(204).json()
-    } catch (error) {
-        if(error instanceof AppError){
-            handleError(error, res)
-        }
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    await updateUserService(id, data);
+    return res.status(204).json();
+  } catch (error) {
+    if (error instanceof AppError) {
+      handleError(error, res);
     }
-}
+  }
+};
 
-export default updateUserController
+export default updateUserController;
